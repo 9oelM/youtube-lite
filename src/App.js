6cc97e078277befba3ca7b9c5b527c78a53bad22
Joel M <hj923@hotmail.com>
2018-06-30 16:57:40 +0000

    separate searchField to another file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import React, { Component } from 'react';
-import YoutubeAutocomplete from 'react-youtube-autocomplete';
-import "./App.css";
+import React, { Component } from 'react'
+import "./styles/output/master.css"
+import SearchField from './components/SearchField'
 
 class App extends Component {
   render() {
-    
-    const API_KEY = "AIzaSyB8R4Bqkx25_-c58L7v1QaLReVw1FWea28",
-          MAX_RESULTS = "20",
-          PLACEHOLDER = "Search video"
-    
     return (
-      <YoutubeAutocomplete
-        apiKey={API_KEY}        // you must get an API key from google if you want video search results returned
-        maxResults={MAX_RESULTS}    // defaults -> 50. Number of video search results you want
-        placeHolder={PLACEHOLDER}   // defaults -> "Search Youtube"
-        callback={this.showResults}    // callback to execute when search results are retrieved
-        className = "search-field"     // defaults -> random string
-      />
-    );
+      <SearchField />
+    )
   }
   
   showResults(results) {
