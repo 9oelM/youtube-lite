ee25eb2bad6bf77d6c1f244a1108a3b6901cd1bc
9oelM <hj923@hotmail.com>
2018-08-10 12:39:24 +0000

    remove thunk middleware for now

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
 import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
 import reducers from '../reducers/reducers'
 
 const store = createStore(
   reducers,
-  applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
