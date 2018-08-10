bac08abba53d5cde4c3abf46f8a17ecade013e58
Joel M <hj923@hotmail.com>
2018-08-10 04:06:55 +0000

    add AppContainer

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
-)
+import { AppContainer } from 'react-hot-loader'
+const render = Component =>
+  ReactDOM.render(
+    <AppContainer>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppContainer>,
+    document.getElementById('root')
+  )
 registerServiceWorker()
+render(App)
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App)
+  })
+}
