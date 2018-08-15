8ca099feea3edad09fdaed897ec6702e3fa0e972
9oelM <hj923@hotmail.com>
2018-08-15 13:36:12 +0000

    fix layout error by adding setContentPagePaddingEqual()

--- src/layout/contentPage.js
+++ src/layout/contentPage.js
@@ -1,16 +1,29 @@
 import Grid from '@material-ui/core/Grid'
 import { TopNavContainer } from '../components/Containers/index.js'
 import React from 'react'
+import { setContentPagePaddingEqual } from '../modules/resize'
 
-const ContentPage = ({ children }) => {
-  return (
-    <React.Fragment>
-      <TopNavContainer />
-      <Grid id="ContentPage" style={{ padding: 16 }}>
-        {children}
-      </Grid>
-    </React.Fragment>
-  )
+class ContentPage extends React.Component {
+  componentDidMount() {
+    setContentPagePaddingEqual()
+  }
+
+  componentDidUpdate() {
+    setContentPagePaddingEqual()
+  }
+
+  render() {
+    const { children } = this.props
+
+    return (
+      <React.Fragment>
+        <TopNavContainer />
+        <Grid id="ContentPage" style={{ padding: 16 }}>
+          {children}
+        </Grid>
+      </React.Fragment>
+    )
+  }
 }
 
 export default ContentPage
