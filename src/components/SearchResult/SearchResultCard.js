a42c97434674eea4f0a39970fdc2f051140d0273
9oelM <hj923@hotmail.com>
2018-08-19 07:20:23 +0000

    add anchor for now

--- src/components/SearchResult/SearchResultCard.js
+++ src/components/SearchResult/SearchResultCard.js
@@ -4,28 +4,31 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import ButtonBase from '@material-ui/core/ButtonBase'
+import Grid from '@material-ui/core/Grid'
 
-const searchResultCard = ({ title, author, img, description }) => {
+const searchResultCard = ({ title, author, img, description, onClick, id }) => {
   return (
-    <ButtonBase className="searchResultCard">
-      <Card square>
-        <CardContent>
-          <Typography align="left" variant="title">
-            {title}
-          </Typography>
-          <Typography
-            align="left"
-            variant="subheading"
-          >{`by ${author}`}</Typography>
-        </CardContent>
-        <CardMedia className="searchResultMedia" image={img} title={title} />
-        <CardContent>
-          <Typography align="left" variant="body1">
-            {description}
-          </Typography>
-        </CardContent>
-      </Card>
-    </ButtonBase>
+    <Grid item>
+      <ButtonBase className="searchResultCard" id={id} onClick={onClick}>
+        <Card square>
+          <CardContent>
+            <Typography align="left" variant="title">
+              {title}
+            </Typography>
+            <Typography
+              align="left"
+              variant="subheading"
+            >{`by ${author}`}</Typography>
+          </CardContent>
+          <CardMedia className="searchResultMedia" image={img} title={title} />
+          <CardContent>
+            <Typography align="left" variant="body1">
+              {description}
+            </Typography>
+          </CardContent>
+        </Card>
+      </ButtonBase>
+    </Grid>
   )
 }
 
--- src/components/SearchResult/SearchResultPanel.js
+++ src/components/SearchResult/SearchResultPanel.js
@@ -2,26 +2,94 @@ import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import Card from './SearchResultCard.js'
 import shortid from 'shortid'
+import Popover from '@material-ui/core/Popover'
+import Typography from '@material-ui/core/Typography'
+import VideoPlayer from '../VideoPlayer/VideoPlayer'
+class SearchResultPanel extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      anchor: null,
+      cards: [],
+      maxCardNum: 0,
+    }
+  }
 
-const SearchResultPanel = ({ searchResults }) => {
-  return (
-    <Grid style={{ padding: 16 }}>
-      <Grid spacing={16} container justify="center">
-        {searchResults.map(item => {
-          return (
-            <Grid item key={shortid.generate()}>
+  componentDidMount() {
+    this.setState({
+      cards: document.getElementsByClassName('searchResultCard'),
+    })
+  }
+
+  handleClick = (event, index) => {
+    let clickedCard = this.findClickedCard(event, this.state.cards)
+    console.log(`left: ${clickedCard.offsetLeft + clickedCard.clientWidth / 2}
+    top: ${clickedCard.offsetTop + clickedCard.clientHeight / 2}
+    `)
+
+    this.setState({
+      anchor: {
+        left: clickedCard.offsetLeft + clickedCard.clientWidth / 2,
+        top: clickedCard.offsetTop + clickedCard.clientHeight / 2,
+      },
+    })
+  }
+
+  handleClose = () => {
+    this.setState({
+      anchor: null,
+    })
+  }
+
+  findClickedCard = (event, cards) => {
+    for (let i = 0; i < cards.length; i++) {
+      if (event.currentTarget.id == cards[i].id) return cards[i]
+    }
+  }
+
+  render() {
+    const { searchResults } = this.props
+    const { anchor } = this.state
+    let open = Boolean(anchor)
+    console.log('A: ' + this.state.anchor)
+    return (
+      <Grid style={{ padding: 16 }}>
+        <Grid spacing={16} container justify="center">
+          {searchResults.map((item, i) => {
+            return (
               <Card
+                key={shortid.generate()}
                 title={item.title}
                 author={item.channelTitle}
                 description={item.description}
                 img={item.thumbnails.medium.url}
+                id={`card-${i}`}
+                vId={item.id}
+                onClick={e => this.handleClick(e, i)}
               />
-            </Grid>
-          )
-        })}
+            )
+          })}
+          <Popover
+            id="simple-popper"
+            open={open}
+            onClose={this.handleClose}
+            anchorOrigin={{
+              vertical: 'center',
+              horizontal: 'center',
+            }}
+            anchorReference="anchorPosition"
+            anchorPosition={anchor}
+            transformOrigin={{
+              vertical: 'center',
+              horizontal: 'center',
+            }}
+          >
+            <VideoPlayer />
+          </Popover>
+        </Grid>
       </Grid>
-    </Grid>
-  )
+    )
+  }
 }
 
 export default SearchResultPanel
