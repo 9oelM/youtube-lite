b3684bd2facb096b6b1e0221f65ec2a2cdd99b55
9oelM <hj923@hotmail.com>
2018-08-22 14:33:45 +0000

    remove useless component

--- src/layout/default.js
+++ src/layout/default.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
-import { TopNavContainer } from '../components/Containers/index.js'
-
 const defaultLayout = ({ children }) => {
   return (
     <Grid item xs={12} id="layout">
