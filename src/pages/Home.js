14fab1aa60eed927cfb7020db2b5925e43bda3db
9oelM <hj923@hotmail.com>
2018-08-22 17:24:39 +0000

    remove everything from home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,18 +7,7 @@ const Home = ({ isEng = true }) => {
   const captionEng = `No more wasting time on watching random, irrelevant videos on youtube. Feel guilty by looking at the statistics you generate.`
   const captionKor = `유투브에서 아무 비디오나 보다가 시간 낭비하는 건 이제 그만. 여러분이 직접 만들어내는 통계를 보고 죄책감을 느끼세요.`
   const caption = isEng ? captionEng : captionKor
-  return (
-    <ContentGrid>
-      <div style={{ padding: '16px' }}>
-        <Card>
-          <CardContent>
-            <Typography variant="headline">Youtube Lite</Typography>
-            <Typography variant="subheading">{caption}</Typography>
-          </CardContent>
-        </Card>
-      </div>
-    </ContentGrid>
-  )
+  return <ContentGrid />
 }
 
 export default Home
