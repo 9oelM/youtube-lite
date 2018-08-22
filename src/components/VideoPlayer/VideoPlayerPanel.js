6eb1a3e90512711e207fd45f1137644b9465d42b
9oelM <hj923@hotmail.com>
2018-08-22 15:50:57 +0000

    add videoPlayerPanel

---
+++ src/components/VideoPlayer/VideoPlayerPanel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import VideoPlayer from './VideoPlayer'
+import Paper from '@material-ui/core/Paper'
+
+const videoPlayerPanel = () => {
+  return (
+    <Paper>
+      <VideoPlayer />
+    </Paper>
+  )
+}
