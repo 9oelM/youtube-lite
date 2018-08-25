1515e0a191c2791eb65b852c54ba7160f5b7712a
9oelM <hj923@hotmail.com>
2018-08-25 14:59:05 +0000

    disallow singlequote

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,7 +5,7 @@ module.exports = {
   useTabs: false, // Indent lines with tabs instead of spaces.
   printWidth: 80, // Specify the length of line that the printer will wrap on.
   tabWidth: 2, // Specify the number of spaces per indentation-level.
-  singleQuote: true, // Use single quotes instead of double quotes.
+  singleQuote: false, // Use single quotes instead of double quotes.
   /**
    * Print trailing commas wherever possible.
    * Valid options:
