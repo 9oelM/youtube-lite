c8611fdc742f80999de7f2d18efab0ef98c788af
9oelM <hj923@hotmail.com>
2018-09-21 16:53:26 +0000

    add bottomnav

---
+++ src/components/BottomNav/BottomNav.js
@@ -0,0 +1,35 @@
+import React from "react"
+import PropTypes from "prop-types"
+import RouterPT from "react-router-prop-types"
+import { withRouter } from "react-router-dom"
+import { withStyles } from "@material-ui/core/styles"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import MenuIcon from "@material-ui/icons/Menu"
+import Drawer from "../Drawer/Drawer"
+
+const BottomNav = ({ TotalVids, TotalTime }) => (
+  <div id="bottom-nav">
+    <AppBar id="bottom-app-bar">
+      <Toolbar id="bottom-toolbar">
+        <Typography
+          variant="caption"
+          color="inherit"
+          className="bottom-nav-status"
+        >
+          Videos watched: {5} | &nbsp;
+        </Typography>
+        <Typography
+          variant="caption"
+          color="inherit"
+          className="bottom-nav-status"
+        >
+          Total time watched: {"1.5 hr"}
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </div>
+)
+
+export default BottomNav
