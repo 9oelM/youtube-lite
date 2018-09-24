bcde62719e70d9f9b0853ff849c87d8cd437a709
9oelM <hj923@hotmail.com>
2018-09-24 14:12:50 +0000

    Add default theme

---
+++ src/Theme.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { createMuiTheme } from "@material-ui/core/styles"
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: "#263238" },
+    secondary: { main: "#c51162" },
+  },
+})
+
+export default theme
