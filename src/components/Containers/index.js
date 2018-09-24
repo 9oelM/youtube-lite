48e1e299debd3cbb587e6c1ef23034a26b4a5690
9oelM <hj923@hotmail.com>
2018-09-24 14:10:45 +0000

    add container for contentgrid

--- src/components/Containers/index.js
+++ src/components/Containers/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { connect } from "react-redux"
 import TopNav from "../TopNav/TopNav"
 import SearchResultPanel from "../SearchResult/SearchResultPanel"
+import ContentGrid from "../Layout/ContentGrid"
 import { toggleDrawer, receiveSearch, requestSearch } from "../../actions/index"
 
 export const TopNavContainer = connect(
@@ -21,6 +22,16 @@ export const TopNavContainer = connect(
   })
 )(TopNav)
 
-export const SearchResultPanelContainer = connect(state => ({
-  searchResults: state.searchReducer.searchResults,
-}))(SearchResultPanel)
+export const SearchResultPanelContainer = connect(
+  state => ({
+    searchResults: state.searchReducer.searchResults,
+  }),
+  null
+)(SearchResultPanel)
+
+export const ContentGridContainer = connect(
+  state => ({
+    isFetching: state.searchReducer.isFetching,
+  }),
+  null
+)(ContentGrid)
