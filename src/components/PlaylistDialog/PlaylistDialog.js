35a0b08b88c2f66f90ab39c4f436a5452006b94d
9oelM <hj923@hotmail.com>
2018-10-13 16:04:53 +0000

    redux add playlist UI

--- src/components/PlaylistDialog/PlaylistDialog.js
+++ src/components/PlaylistDialog/PlaylistDialog.js
@@ -4,15 +4,30 @@ import Dialog from "@material-ui/core/Dialog"
 import List from "@material-ui/core/List"
 import ListItem from "@material-ui/core/ListItem"
 import ListItemText from "@material-ui/core/ListItemText"
+import Input from "@material-ui/core/Input"
+import Modal from "@material-ui/core/Modal"
+import Button from "@material-ui/core/Button"
 import shortid from "shortid"
 
 class PlaylistDialog extends React.Component {
-  handleClose = () => {
-    this.props.onClose(this.props.selectedValue)
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputVisible: false,
+      inputValue: "",
+    }
   }
 
-  handleListItemClick = value => {
-    this.props.onClose(value)
+  handleChange = () => {
+    this.setState({
+      inputVisible: !this.state.inputVisible,
+    })
+  }
+
+  handleInputChange = evt => {
+    this.setState({
+      inputValue: evt.target.value,
+    })
   }
 
   render() {
@@ -24,6 +39,7 @@ class PlaylistDialog extends React.Component {
       onAddToPlaylist,
       onAddPlaylist,
     } = this.props
+    const { inputVisible, inputValue } = this.state
     return (
       <Dialog onClose={onClose} open={open}>
         <DialogTitle>Playlists</DialogTitle>
@@ -40,10 +56,38 @@ class PlaylistDialog extends React.Component {
                 <ListItemText primary={item.playlistName} />
               </ListItem>
             ))}
-            <ListItem button onClick={f => f}>
+            <ListItem button onClick={this.handleChange}>
               <ListItemText primary="Create new playlist" />
             </ListItem>
           </List>
+          <Dialog open={inputVisible}>
+            <DialogTitle>Create new playlist</DialogTitle>
+            <List>
+              <ListItem className="create-new-playlist">
+                <Input
+                  placeholder="Enter name"
+                  inputProps={{
+                    "aria-label": "Playlist",
+                  }}
+                  onChange={this.handleInputChange}
+                />
+                <div className="create-new-playlist-control">
+                  <Button
+                    color="primary"
+                    onClick={() => {
+                      onAddPlaylist(inputValue)
+                      this.handleChange()
+                    }}
+                  >
+                    Add
+                  </Button>
+                  <Button color="secondary" onClick={this.handleChange}>
+                    Cancel
+                  </Button>
+                </div>
+              </ListItem>
+            </List>
+          </Dialog>
         </div>
       </Dialog>
     )
