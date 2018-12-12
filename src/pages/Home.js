52d21fa95a6937924963d4baa39f3bdb80940be5
9oelM <hj923@hotmail.com>
2018-12-12 13:17:02 +0000

    use containers instead of mere components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,31 +3,27 @@ import PropTypes from "prop-types"
 import Typography from "@material-ui/core/Typography"
 import Paper from "@material-ui/core/Paper"
 import ContentGrid from "../layout/ContentGrid"
+import {
+  TotalVideosWatchedContainer,
+  TotalTimeWatchedContainer,
+} from "../components/Containers/index"
 
-const Home = ({ videosWatched = "5", totalTimeWatched = "1.5 hours" }) => {
+const Home = () => {
   return (
     <ContentGrid _className="home">
       <Typography variant="display1" align="left">
         You have watched
       </Typography>
-      <Typography variant="display4" align="left">
-        {videosWatched} videos{" "}
-      </Typography>
+      <TotalVideosWatchedContainer />
       <Typography variant="display1" align="left">
         for
       </Typography>
-      <Typography variant="display4" align="left">
-        {totalTimeWatched}
-      </Typography>
+      <TotalTimeWatchedContainer />
       <Typography variant="display1" align="left">
         over the past 24 hours.
       </Typography>
     </ContentGrid>
   )
-}
-
-Home.propTypes = {
-  isEng: PropTypes.bool.isRequired,
 }
 
 export default Home
