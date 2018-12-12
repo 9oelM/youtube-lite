b74cf3a19322280589c9fb5c709ef72b3d5c5269
9oelM <hj923@hotmail.com>
2018-12-12 09:55:04 +0000

    add channels page

---
+++ src/pages/Channels.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Typography from "@material-ui/core/Typography"
+import ContentGrid from "../layout/ContentGrid"
+
+const Channels = () => {
+  return (
+    <ContentGrid _className="home">
+      <Typography variant="display1" align="left">
+        Channels page is under construction!
+      </Typography>
+    </ContentGrid>
+  )
+}
+
+export default Channels
