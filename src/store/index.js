131439716304f9e51cb970de51b605c5b7f42725
9oelM <hj923@hotmail.com>
2018-12-15 07:19:58 +0000

    add persisting functionalities to sync with localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
-import { createStore, applyMiddleware } from "redux"
+import { createStore } from "redux"
 import reducers from "../reducers/reducers"
+import { persistStore } from "redux-persist"
 
 const store = createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
-
-export default store
+const persistor = persistStore(store)
+export { store, persistor }
