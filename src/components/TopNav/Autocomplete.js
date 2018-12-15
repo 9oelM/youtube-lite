9f0b80b72a71a2b847ed6335df8df774fd4100b8
9oelM <hj923@hotmail.com>
2018-12-15 07:23:07 +0000

    lint fix

--- src/components/TopNav/Autocomplete.js
+++ src/components/TopNav/Autocomplete.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Downshift from 'downshift'
-import PropTypes from 'prop-types'
-import Input from '@material-ui/core/Input'
-import Paper from '@material-ui/core/Paper'
-import MenuItem from '@material-ui/core/MenuItem'
-import jsonp from 'jsonp'
-import shortid from 'shortid'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
-import blue from '@material-ui/core/colors/blue'
-import searchYoutube from 'youtube-search'
+import React from "react"
+import Downshift from "downshift"
+import PropTypes from "prop-types"
+import Input from "@material-ui/core/Input"
+import Paper from "@material-ui/core/Paper"
+import MenuItem from "@material-ui/core/MenuItem"
+import jsonp from "jsonp"
+import shortid from "shortid"
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import blue from "@material-ui/core/colors/blue"
+import searchYoutube from "youtube-search"
 
 class Autocomplete extends React.Component {
   constructor(props) {
@@ -20,7 +20,7 @@ class Autocomplete extends React.Component {
     this.getSearchSuggestions = this.getSearchSuggestions.bind(this)
     this.handleOnSelect = this.handleOnSelect.bind(this)
     this.state = {
-      inputValue: '',
+      inputValue: "",
       searchSuggestions: [],
       isMenuOpen: true,
     }
@@ -37,12 +37,12 @@ class Autocomplete extends React.Component {
   fetchSuggestionResults(query) {
     const self = this,
       googleAutoSuggestURL =
-        '//suggestqueries.google.com/complete/search?client=youtube&ds=yt&q='
+        "//suggestqueries.google.com/complete/search?client=youtube&ds=yt&q="
 
     jsonp(`${googleAutoSuggestURL}${query}`, function(error, data) {
       if (error && self.props.onSuggestError) {
-          self.props.onSuggestError(error)
-          return
+        self.props.onSuggestError(error)
+        return
       }
       let searchResult = data[1]
 
@@ -52,11 +52,11 @@ class Autocomplete extends React.Component {
 
   fetchSearchResults(searchWord) {
     const self = this
-    const {option}= this.props
+    const { option } = this.props
 
     let opt = {
       maxResults: 15,
-      ...option
+      ...option,
     }
 
     searchYoutube(searchWord, opt, function(err, results) {
@@ -64,128 +64,139 @@ class Autocomplete extends React.Component {
         self.props.onSearchError(err)
         return
       }
-      self.props.onSearchResults? self.props.onSearchResults(results) : ()=>{}
+      self.props.onSearchResults
+        ? self.props.onSearchResults(results)
+        : () => {}
     })
   }
 
   handleInputValueChange(_inputValue) {
     this.fetchSuggestionResults(_inputValue)
-    
+
     this.setState({ inputValue: _inputValue })
   }
 
   handleItemToString(item) {
-    return item ? item.text : ''
+    return item ? item.text : ""
   }
-  
-  handleOnSelect(selectedItem, stateAndHelpers){
+
+  handleOnSelect(selectedItem, stateAndHelpers) {
     const { onSearchTrigger } = this.props
     this.fetchSearchResults(selectedItem.text)
     this.setState({ isMenuOpen: false })
-    onSearchTrigger ? onSearchTrigger(selectedItem.text) : f=>f
+    onSearchTrigger ? onSearchTrigger(selectedItem.text) : f => f
   }
 
   render() {
     const { searchSuggestions, isMenuOpen } = this.state
     const {
       useMui = true,
-      inputId = 'youtube-autocomplete-input',
-      menuId = 'youtube-autocomplete-menu',
-      itemClassName = 'youtube-autocomplete-items',
+      inputId = "youtube-autocomplete-input",
+      menuId = "youtube-autocomplete-menu",
+      itemClassName = "youtube-autocomplete-items",
       theme = createMuiTheme({
         primary: blue,
       }),
-      placeholderText = 'Search youtube',
-      onSearchTrigger
+      placeholderText = "Search youtube",
+      onSearchTrigger,
     } = this.props
 
     return (
-      <div 
-        id="youtube-autocomplete">
-      <Downshift
-        onInputValueChange={this.handleInputValueChange}
-        itemToString={this.handleItemToString}
-        isOpen={isMenuOpen}
-        onSelect={this.handleOnSelect}
-      >
-        {({ getInputProps, getItemProps, getMenuProps, isOpen, onKeyDown }) => (
-          <div>
-            {useMui ? (
-              <MuiThemeProvider theme={theme}>
-                <Input
-                  {...getInputProps({
-                    placeholder: placeholderText,
-                    fullWidth: true,
-                    onKeyDown: e => {
-                      this.setState({ isMenuOpen: true })
-                      if (e.key === 'Enter') {
-                        this.fetchSearchResults(this.state.inputValue)
-                        this.setState({ isMenuOpen: false })
-                        onSearchTrigger ? onSearchTrigger(this.state.inputValue) : f=>f
-                      }
-                    },
-                  })}
-                />
-                {isOpen ? (
-                  <Paper square {...getMenuProps()}>
-                    {searchSuggestions.map((item, index) => (
-                      <MenuItem
-                        {...getItemProps({
-                          key: item.id,
-                          index,
-                          item,
-                          style: {
-                            zIndex: 1,
-                          },
-                        })}
-                      >
-                        {item.text}
-                      </MenuItem>
-                    ))}
-                  </Paper>
-                ) : null}
-              </MuiThemeProvider>
-            ) : (
-              <React.Fragment>
-                <input
-                  id={inputId}
-                  {...getInputProps({
-                    placeholder: placeholderText,
-                    fullWidth: true,
-                    onKeyDown: e => {
-                      this.setState({ isMenuOpen: true })
-                      if (e.key === 'Enter') {
-                        this.fetchSearchResults(this.state.inputValue)
-                        this.setState({ isMenuOpen: false })
-                        onSearchTrigger ? onSearchTrigger(this.state.inputValue) : f=>f
-                      }
-                    },
-                  })}
-                />
-                {isOpen ? (
-                  <div id={menuId} {...getMenuProps()}>
-                    {searchSuggestions.map((item, index) => (
-                      <div
-                        className={itemClassName}
-                        {...getItemProps({
-                          key: item.id,
-                          index,
-                          item,
-                          style: {
-                            zIndex: 1,
-                          },
-                        })}
-                      >
-                        {item.text}
-                      </div>
-                    ))}
-                  </div>
-                ) : null}
-              </React.Fragment>
-            )}
-          </div>
-        )}
-      </Downshift>
+      <div id="youtube-autocomplete">
+        <Downshift
+          onInputValueChange={this.handleInputValueChange}
+          itemToString={this.handleItemToString}
+          isOpen={isMenuOpen}
+          onSelect={this.handleOnSelect}
+        >
+          {({
+            getInputProps,
+            getItemProps,
+            getMenuProps,
+            isOpen,
+            onKeyDown,
+          }) => (
+            <div>
+              {useMui ? (
+                <MuiThemeProvider theme={theme}>
+                  <Input
+                    {...getInputProps({
+                      placeholder: placeholderText,
+                      fullWidth: true,
+                      onKeyDown: e => {
+                        this.setState({ isMenuOpen: true })
+                        if (e.key === "Enter") {
+                          this.fetchSearchResults(this.state.inputValue)
+                          this.setState({ isMenuOpen: false })
+                          onSearchTrigger
+                            ? onSearchTrigger(this.state.inputValue)
+                            : f => f
+                        }
+                      },
+                    })}
+                  />
+                  {isOpen ? (
+                    <Paper square {...getMenuProps()}>
+                      {searchSuggestions.map((item, index) => (
+                        <MenuItem
+                          {...getItemProps({
+                            key: item.id,
+                            index,
+                            item,
+                            style: {
+                              zIndex: 1,
+                            },
+                          })}
+                        >
+                          {item.text}
+                        </MenuItem>
+                      ))}
+                    </Paper>
+                  ) : null}
+                </MuiThemeProvider>
+              ) : (
+                <React.Fragment>
+                  <input
+                    id={inputId}
+                    {...getInputProps({
+                      placeholder: placeholderText,
+                      fullWidth: true,
+                      onKeyDown: e => {
+                        this.setState({ isMenuOpen: true })
+                        if (e.key === "Enter") {
+                          this.fetchSearchResults(this.state.inputValue)
+                          this.setState({ isMenuOpen: false })
+                          onSearchTrigger
+                            ? onSearchTrigger(this.state.inputValue)
+                            : f => f
+                        }
+                      },
+                    })}
+                  />
+                  {isOpen ? (
+                    <div id={menuId} {...getMenuProps()}>
+                      {searchSuggestions.map((item, index) => (
+                        <div
+                          className={itemClassName}
+                          {...getItemProps({
+                            key: item.id,
+                            index,
+                            item,
+                            style: {
+                              zIndex: 1,
+                            },
+                          })}
+                        >
+                          {item.text}
+                        </div>
+                      ))}
+                    </div>
+                  ) : null}
+                </React.Fragment>
+              )}
+            </div>
+          )}
+        </Downshift>
       </div>
     )
   }
