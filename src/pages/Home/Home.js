36e7ef8ad488042e0ffdb591acb84c74adc83a50
9oelM <hj923@hotmail.com>
2019-01-01 18:46:53 +0000

    Move home into foler

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,24 +2,25 @@ import React from "react"
 import PropTypes from "prop-types"
 import Typography from "@material-ui/core/Typography"
 import Paper from "@material-ui/core/Paper"
-import ContentGrid from "../layout/ContentGrid"
+import ContentGrid from "../../layout/ContentGrid"
 import {
   TotalVideosWatchedContainer,
   TotalTimeWatchedContainer,
-} from "../components/Containers/index"
+} from "../../components/Containers/index"
+import "./Home.sass"
 
 const Home = () => {
   return (
     <ContentGrid _className="home">
-      <Typography variant="display1" align="left">
+      <Typography variant="h5" align="center">
         You have watched
       </Typography>
       <TotalVideosWatchedContainer />
-      <Typography variant="display1" align="left">
+      <Typography variant="h5" align="center">
         for
       </Typography>
       <TotalTimeWatchedContainer />
-      <Typography variant="display1" align="left">
+      <Typography variant="h5" align="center">
         over the past 24 hours.
       </Typography>
     </ContentGrid>
