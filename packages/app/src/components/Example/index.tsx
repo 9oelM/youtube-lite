380939148b34a319644a8426d928e1f304c1689b
9oelM <hj923@hotmail.com>
2021-05-29 20:35:10 +0900

    ADD: basic working setup

---
+++ packages/app/src/components/Example/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { FC } from 'react';
+
+export const Example: FC = () => <div>test</div>
---
+++ packages/app/src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Example } from './components/Example';
+
+ReactDOM.render(<Example />, document.getElementById('root'));
