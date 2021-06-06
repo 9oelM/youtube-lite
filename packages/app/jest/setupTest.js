331830cab54ff0a903ec2b53c51ca6171d754d6e
9oelM <hj923@hotmail.com>
2021-06-06 21:43:54 +0900

    CHG: migrate to lerna and use react^16

--- packages/app/jest/setupTest.js
+++ packages/app/jest/setupTest.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const Enzyme = require(`enzyme`)
-const Adapter = require(`@wojtekmaj/enzyme-adapter-react-17`)
+const Adapter = require(`enzyme-adapter-react-16`)
 
 Enzyme.configure({ adapter: new Adapter() })
