7fcc4cfc9122d6dde6d4e9f3904fc4d85d4f423d
9oelM <hj923@hotmail.com>
2021-06-15 13:35:03 +0900

    ADD: initial work on SearchResultsPage

---
+++ packages/app/src/components/Pages/SearchResult/index.tsx
@@ -0,0 +1,53 @@
+import { x } from "@xstyled/styled-components"
+import { push } from "connected-react-router"
+import React, { useEffect } from "react"
+import { FC } from "react"
+import { useDispatch } from "react-redux"
+import { useLocation } from "react-router-dom"
+import { SearchResultPageFallback } from "src/components/Pages/SearchResult/fallback"
+import { SearchResultPageSearchInputImpure } from "src/components/Pages/SearchResult/localFragments/SearchResultPageSearchInput"
+import { Jobs } from "src/redux/ducks/async/asyncActions"
+import { SF } from "src/styles/styleFragments"
+import { enhance } from "src/utilities/essentials"
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type SearchResultPageImpureProps = {}
+
+export const SearchResultPageImpure: FC<SearchResultPageImpureProps> =
+  enhance<SearchResultPageImpureProps>(() => {
+    const query = new URLSearchParams(useLocation().search)
+    const dispatch = useDispatch()
+    const queryString = query.get(`search_query`)
+
+    useEffect(() => {
+      if (!queryString) {
+        dispatch(push(`/`))
+
+        return
+      }
+
+      dispatch(
+        Jobs.getSearchResult.start({
+          payload: {
+            searchQuery: queryString,
+          },
+        })
+      )
+    }, [dispatch, queryString])
+
+    return <SearchResultPagePure />
+  })(SearchResultPageFallback)
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type SearchResultPagePureProps = {}
+
+export const SearchResultPagePure: FC<SearchResultPagePureProps> =
+  enhance<SearchResultPagePureProps>(() => {
+    return (
+      <x.main {...SF.flexStyles} {...SF.fullWH} bg="background">
+        <x.nav position="absolute" w={2 / 3} top="25px">
+          <SearchResultPageSearchInputImpure />
+        </x.nav>
+      </x.main>
+    )
+  })(SearchResultPageFallback)
