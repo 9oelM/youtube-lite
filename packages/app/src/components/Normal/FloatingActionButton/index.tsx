061d49f82bd49bb32fa641d918285297d42e7ea9
변주영 <wndudqus@naver.com>
2021-06-15 22:56:39 +0900

    MenuType 지우고 각각 onClick 구현하도록 처리

--- packages/app/src/components/Normal/FloatingActionButton/index.tsx
+++ packages/app/src/components/Normal/FloatingActionButton/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useCallback, useState } from "react"
+import React, { ReactNode, useCallback, useMemo, useState } from "react"
 import { FC } from "react"
 import { enhance } from "src/utilities/essentials"
 import { FloatingActionButtonFallback } from "./fallback"
@@ -12,15 +12,15 @@ import { Information } from "styled-icons/ionicons-sharp"
 
 export type Menu = {
   icon: ReactNode
-  menuType: MenuType
+  onClick: () => void
 }
 
-enum MenuType {
-  DEFAULT,
-  PLAYLIST,
-  SETTINGS,
-  INFO,
-}
+// enum MenuType {
+//   DEFAULT,
+//   PLAYLIST,
+//   SETTINGS,
+//   INFO,
+// }
 
 export type FloatingActionButtonImpureProps = {
   //
@@ -34,36 +34,29 @@ export const FloatingActionButtonImpure: FC<FloatingActionButtonImpureProps> =
       setIsActive((prev) => !prev)
     }, [])
 
-    const menus = [
-      {
-        icon: <PlaylistPlay />,
-        menuType: MenuType.PLAYLIST,
-      },
-      {
-        icon: <Settings />,
-        menuType: MenuType.SETTINGS,
-      },
-      {
-        icon: <Information />,
-        menuType: MenuType.INFO,
-      },
-    ]
-
-    const onClickMenu = useCallback((menuType?: MenuType) => {
-      switch (menuType) {
-        case MenuType.INFO:
-          break
-
-        case MenuType.PLAYLIST:
-          break
-
-        case MenuType.SETTINGS:
-          break
-
-        default:
-          break
-      }
-    }, [])
+    const menus = useMemo(
+      () => [
+        {
+          icon: <PlaylistPlay />,
+          onClick: () => {
+            //
+          },
+        },
+        {
+          icon: <Settings />,
+          onClick: () => {
+            //
+          },
+        },
+        {
+          icon: <Information />,
+          onClick: () => {
+            //
+          },
+        },
+      ],
+      []
+    )
 
     return (
       <x.div>
@@ -77,9 +70,8 @@ export const FloatingActionButtonImpure: FC<FloatingActionButtonImpureProps> =
           <FloatingActionButtonPure
             key={idx}
             icon={menu.icon}
-            onClick={onClickMenu}
+            onClick={menu.onClick}
             active={isActive}
-            menuType={menu.menuType}
             index={idx}
           />
         ))}
@@ -89,29 +81,23 @@ export const FloatingActionButtonImpure: FC<FloatingActionButtonImpureProps> =
 
 export type FloatingActionButtonPureProps = {
   icon: ReactNode
-  onClick: (menuType?: MenuType) => void
-  menuType?: MenuType
+  onClick: () => void
   active: boolean
   index: number
 }
 
 export const FloatingActionButtonPure: FC<FloatingActionButtonPureProps> =
-  enhance<FloatingActionButtonPureProps>(
-    ({ icon, onClick, menuType, active, index }) => {
-      const onClickButton = () => {
-        onClick(menuType)
-      }
-      return (
-        <x.button
-          onClick={onClickButton}
-          {...V.buttons.floating}
-          w={active ? `55` : `0`}
-          h={active ? `55` : `0`}
-          opacity={active ? `1` : `0`}
-          transform={`translateX(50%) translateY(-${(index + 1) * 150}%)`}
-        >
-          {icon}
-        </x.button>
-      )
-    }
-  )(FloatingActionButtonFallback)
+  enhance<FloatingActionButtonPureProps>(({ icon, onClick, active, index }) => {
+    return (
+      <x.button
+        onClick={onClick}
+        {...V.buttons.floating}
+        w={active ? `55` : `0`}
+        h={active ? `55` : `0`}
+        opacity={active ? `1` : `0`}
+        transform={`translateX(50%) translateY(-${(index + 1) * 150}%)`}
+      >
+        {icon}
+      </x.button>
+    )
+  })(FloatingActionButtonFallback)
