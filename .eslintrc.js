ed42414d961e330834eafa2940decf7478393d0d
9oelM <hj923@hotmail.com>
2021-06-15 13:34:02 +0900

    ADD(deps): eslint-plugin-react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     `plugin:@typescript-eslint/eslint-recommended`,
     `plugin:@typescript-eslint/recommended`,
     `plugin:react/recommended`,
+    `plugin:react-hooks/recommended`,
     `prettier`,
   ],
   // https://github.com/yannickcr/eslint-plugin-react#configuration
