ea5684c8a68d14e850cf7f915613b4eea20853bb
9oelM <hj923@hotmail.com>
2021-06-17 23:37:48 +0900

    ADD(test): more test cases for redux-async

---
+++ packages/app/src/utilities/redux-async/asyncTypes.test.ts
@@ -0,0 +1,66 @@
+import { createJob, failJob } from "src/utilities/redux-async/asyncActions"
+import {
+  asyncActionTypeCreator,
+  isSpecificAsyncActionType,
+  JobActions,
+  REDUX_ASYNC_PREFIX,
+} from "src/utilities/redux-async/asyncTypes"
+
+describe(`asyncActionTypeCreator`, () => {
+  it(`produces expected combination of strings`, () => {
+    expect(asyncActionTypeCreator(JobActions.START, `B`)).toEqual(
+      `${REDUX_ASYNC_PREFIX}/${JobActions.START}/B`
+    )
+  })
+})
+
+describe(`isSpecificAsyncActionType`, () => {
+  it.each([
+    {
+      action: { type: `RANDOM` },
+      jobAction: JobActions.CANCEL,
+      jobName: `TEST`,
+    },
+    {
+      action: { payload: { a: 1 }, type: JobActions.REMOVE },
+      jobAction: JobActions.REMOVE,
+      jobName: `LOGIN`,
+    },
+    {
+      action: failJob({
+        name: `1`,
+        payload: { "11": 1, "22": 3 },
+        id: `QQQQQ`,
+      }),
+      jobAction: JobActions.FAIL,
+      jobName: `123`,
+    },
+  ])(
+    `should return false if action is not an async action type`,
+    ({ action, jobAction, jobName }) => {
+      expect(isSpecificAsyncActionType(action, jobAction, jobName)).toBe(false)
+    }
+  )
+
+  it.each([
+    {
+      action: createJob({ name: `YOUTUBE_LITE`, payload: { is: `awesome` } }),
+      jobAction: JobActions.CREATE,
+      jobName: `YOUTUBE_LITE`,
+    },
+    {
+      action: failJob({
+        name: `1`,
+        payload: { "11": 1, "22": 3 },
+        id: `QQQQQ`,
+      }),
+      jobAction: JobActions.FAIL,
+      jobName: `1`,
+    },
+  ])(
+    `should return true if action is an async action type`,
+    ({ action, jobAction, jobName }) => {
+      expect(isSpecificAsyncActionType(action, jobAction, jobName)).toBe(true)
+    }
+  )
+})
