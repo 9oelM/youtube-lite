b7baf3c13621ed9af2eb9e06f6c074500b12f8d6
9oelM <hj923@hotmail.com>
2021-07-02 00:22:20 +0900

    ADD: search result failure UI

--- packages/app/src/components/Pages/SearchResult/index.tsx
+++ packages/app/src/components/Pages/SearchResult/index.tsx
@@ -83,7 +83,33 @@ export const SearchResultPagePure: FC<SearchResultPagePureProps> =
               return <SearchResultSkeletonLoadingImpure />
             }
             case AsyncStatus.FAILURE: {
-              return <x.div>oops. request failed</x.div>
+              return (
+                <x.section
+                  maxW={{ _: 0.9, md: 0.7 }}
+                  {...SF.flexStyles}
+                  flexDirection="column"
+                  divideY={15}
+                  divideColor="background"
+                >
+                  <x.p color="red-500" textAlign="center">
+                    Request failed.
+                  </x.p>
+                  <x.p color="text" textAlign="center">
+                    This may be because the API key Youtube Lite is providing
+                    has reached the maximum quota for today.{` `}
+                  </x.p>
+                  <x.p
+                    textDecoration="underline"
+                    cursor="pointer"
+                    color={{ _: `text`, hover: `accent` }}
+                    transition="color 0.3s"
+                    textAlign="center"
+                  >
+                    To prevent this kind of issue in the future, consider adding
+                    your own API key.
+                  </x.p>
+                </x.section>
+              )
             }
             case AsyncStatus.SUCCESS: {
               return (
